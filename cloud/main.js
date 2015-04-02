ebd836adcfb332191caf43b4ebb82903bda91112
Thomas Pinella <txpinella@gmail.com>
2015-04-02 17:01:44 -0400

    Update main.js
    Created delete reviews job.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,31 @@
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+//Parse.Cloud.define("hello", function(request, response) {
+  //response.success("Hello world!");
+//});
+
+Parse.Cloud.job("dropData", function(request, status) {
+    Parse.Cloud.useMasterKey();
+    ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Reviews");
+        query.findInBackground(new FindCallback<ParseObject>() {
+        public void done(List<ParseObject> reviews, ParseException e) {
+            if (e == null) {
+                try {
+                   ParseObject.delete(reviews);
+                }catch(ParseException pe) { pe.printStackTrace(); }
+    
+                // OR (do not use both!)
+    
+                // iterate over all messages and delete them
+                for(ParseObject review : reviews)
+                {
+                     review.deleteEventually();
+                }
+            } else {
+                Log.d("Semothing went wrong. ", e.getMessage());
+            }
+    }
 });
+
+}
