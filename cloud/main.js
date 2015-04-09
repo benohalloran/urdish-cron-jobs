05f270a5ceb33ec8da23b0c16d91db55d6630703
Ben O'Halloran <ben@ohalloran.io>
2015-04-09 16:38:54 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,33 +1,16 @@
-//Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-//Parse.Cloud.define("hello", function(request, response) {
-  //response.success("Hello world!");
-//});
-
-Parse.Cloud.job("dropData", function(request, status) {
-    Parse.Cloud.useMasterKey();
-    var Reviews = Parse.Object.extend("Reviews");
-    var query = new Parse.Query(Parse.Reviews);
-    
-    query.find({
-      success: function(results) {
-      
-        for (var i = 0; i < results.length; i++) { 
-          var object = results[i];
-          object.destroy({
-            success: function(object) {
-              // The object was deleted from the Parse Cloud.
-            },
-            error: function(object, error) {
-              // The delete failed.
-              // error is a Parse.Error with an error code and message.
-            }
-          });
-        } 
-      },
-      error: function(error) {
-        alert("Error: " + error.code + " " + error.message);
-      }
-    });
-
-}
+Parse.Cloud.job("Purge", function(request, status) {
+  // Set up to modify user data
+  Parse.Cloud.useMasterKey();
+  var counter = 0;
+  // Query for all users
+  var query = new Parse.Query("Reviews");
+  query.each(function(user) {
+      return user.delete();
+  }).then(function() {
+    // Set the job's success status
+    status.success("Migration completed successfully.");
+  }, function(error) {
+    // Set the job's error status
+    status.error("Uh oh, something went wrong.");
+  });
+});
